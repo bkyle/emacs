f9c43b5c9569b9b0c74bd931ae346e48416a1b64
U-BKYLE\bkyle <bkyle@bkyle.torolab.ibm.com>
2008-05-28 08:30:38 -0700

    Updated with work configuration

--- lisp/config.el
+++ lisp/config.el
@@ -1,3 +1,13 @@
 (setq make-backup-files nil)
 (tool-bar-mode nil)
 (partial-completion-mode t)
+(setq dired-recursive-deletes t)
+
+(require 'highlight-current-line)
+(highlight-current-line-on t)
+(set-face-background 'highlight-current-line-face "light yellow")
+
+; Tabbing...
+(setq-default indent-tabs-mode t)
+(setq-default default-tab-width 4)
+(setq-default tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
--- lisp/modes.el
+++ lisp/modes.el
@@ -4,3 +4,9 @@
 ;; js2-mode
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(setq js2-basic-offset 4)
+(setq js2-mirror-mode nil)
+
+;; C-Mode
+(setq c-basic-offset 4)
+(setq c-default-style '((java-mode . "k&r")))
