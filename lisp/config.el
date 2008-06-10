e4f918c689ccb427d096cb2197897b250a96ca70
U-BKYLE\bkyle <bkyle@bkyle.torolab.ibm.com>
2008-06-10 14:59:58 -0700

    Indentation now will only insert spaces

--- lisp/config.el
+++ lisp/config.el
@@ -8,6 +8,6 @@
 (set-face-background 'highlight-current-line-face "light yellow")
 
 ; Tabbing...
-(setq-default indent-tabs-mode t)
+(setq-default indent-tabs-mode nil)
 (setq-default default-tab-width 4)
 (setq-default tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
