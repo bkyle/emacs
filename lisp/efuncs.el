66c7ef653e8e0d0c0fdcc67f36b2e86d73f86e8b
Bryan Kyle <bkyle@starscream.local>
2008-06-18 22:16:47 -0700

    added diff.sh, started maven functions

--- lisp/efuncs.el
+++ lisp/efuncs.el
@@ -1,3 +1,4 @@
+
 ;;
 ;; Tags
 ;;
@@ -10,15 +11,15 @@
   (interactive "sTag:")
   (if (and (stringp tag) (> (length tag) 0))
       (progn
-	(setq tag-stack (cons tag tag-stack))
-	(insert "<"  tag ">"))))
+        (setq tag-stack (cons tag tag-stack))
+        (insert "<"  tag ">"))))
 
 ;; Removes the top tag from the stack and writes the closing tag to the buffer at the point.
 (defun pop-tag ()
   (interactive)
   (let ((tag (car tag-stack)))
     (if (and (stringp tag) (> (length tag) 0))
-	(insert "</" tag ">")))
+        (insert "</" tag ">")))
   (setq tag-stack (cdr tag-stack)))
 
 ;; Wraps the region in the specified tag.
@@ -39,3 +40,32 @@
     (end-of-line)
     (pop-tag)))
 
+
+;;
+;; Maven
+;;
+
+(defun maven-find-pom (path)
+  (let ((current-path (file-name-as-directory path))
+        (next-path nil)
+        (found-p nil))
+    (catch 'done
+      (while (not found-p)
+        (let ((files (directory-files current-path t))
+              (next-path nil))
+          (dolist (file files)
+            (cond
+             ((equal "pom.xml" (file-name-nondirectory file))
+              (setq found-p t)
+              (throw 'done t))
+             ((equal ".." (file-name-nondirectory file))
+              (setq next-path file))))
+          (if (not next-path)
+              (throw 'done t))
+          (setq current-path next-path)
+          (setq next-path nil))))
+    (cond
+     (found-p
+      (expand-file-name (concat (file-name-as-directory current-path) "pom.xml")))
+     (t
+      (message "Couldn't find pom.xml")))))
