ce87d437d58aff25e8bd43c3d90c0334e3b01b99
Bryan Kyle <bryan.kyle@gmail.com>
2009-07-07 10:41:49 -0700

    Cleaned up modes.el

--- lisp/modes.el
+++ lisp/modes.el
@@ -4,5 +4,6 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.clj" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.scala" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.iwd$" . archive-mode))
