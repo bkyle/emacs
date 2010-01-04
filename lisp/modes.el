889bc73c8f42133702aac8c8e34e28330188ec12
Bryan Kyle <bryan.kyle@gmail.com>
2010-01-04 14:50:17 -0800

    Added auto-mode for Rakefiles

--- lisp/modes.el
+++ lisp/modes.el
@@ -3,6 +3,7 @@
 ;(add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
